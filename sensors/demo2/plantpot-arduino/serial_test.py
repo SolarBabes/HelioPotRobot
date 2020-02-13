eea0533ab93f3cd3a1c7cf084c814a40cd042f51
azamkhan99 <azan_khan99@outlook.com>
2020-02-13 14:05:10 +0000

    removed firebase code

--- sensors/demo2/plantpot-arduino/serial_test.py
+++ sensors/demo2/plantpot-arduino/serial_test.py
@@ -1,14 +1,12 @@
 import serial
 import time
-from firebase import firebase
+
 ser = serial.Serial('/dev/ttyACM0', 9600)
 
-firebase = firebase.FirebaseApplication('https://solarbabesdb.firebaseio.com/bot/plant1')
+
 
 
 while True:
         read_serial = ser.readline()
         cookedserial = read_serial.decode('utf-8')
-        print cookedserial
-        #print read_serial
-        result = firebase.post('/moisture',{time.time():54})
+        print (cookedserial)
