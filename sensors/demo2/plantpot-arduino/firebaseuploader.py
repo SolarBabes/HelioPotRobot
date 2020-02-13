cec50bafd82a86a77f235d195c6ffb4b47ff7a8e
azamkhan99 <46198924+azamkhan99@users.noreply.github.com>
2020-02-13 14:23:29 +0000

    Update firebaseuploader.py

--- sensors/demo2/plantpot-arduino/firebaseuploader.py
+++ sensors/demo2/plantpot-arduino/firebaseuploader.py
@@ -2,15 +2,25 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 import time
+import serial
+from serial import Serial
 TEMPERATURE_STRING = "temperature"
 HUMIDITY_STRING = "humidity"
 LIGHT_STRING = "light"
 MOISTURE_STRING = "moisture"
 
+ser = serial.Serial('/dev/ttyACM0', 9600)
 
-def update(name, value):
-    ref.child("realtime").update({name: value})
-    ref.child(name).update({str(round(time.time())): value})
+
+
+def update(name):
+     while True:
+            read_serial = ser.readline()
+            cookedserial = read_serial.decode('utf-8')
+            print (cookedserial)
+            #return cookedserial
+            ref.child("realtime").update({name: cookedserial})
+            ref.child(name).update({str(round(time.time())): cookedserial})
 
 
 cred = credentials.Certificate('serviceAccountKey.json')
@@ -18,4 +28,4 @@ firebase_admin.initialize_app(cred, {'databaseURL': 'https://solarbabesdb.fireba
 
 plantName = "plant2"  # TODO change here for robot name
 ref = db.reference('bot/'+plantName)
-update(HUMIDITY_STRING, 123)   # TODO change here for sensor name and value
+update(MOISTURE_STRING)   # TODO change here for sensor name and value
