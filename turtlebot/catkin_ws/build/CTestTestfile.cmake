93c57de5cc843d7db8fef25513586449eaff75f2
manavsingh415 <msingh415@hotmail.com>
2020-02-14 14:11:39 +0000

    Add catkin backup

---
+++ turtlebot/catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,11 @@
+# CMake generated Testfile for 
+# Source directory: /home/manav/catkin_ws/src
+# Build directory: /home/manav/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(slam_gmapping/slam_gmapping)
+subdirs(turtlebot3_msgs)
+subdirs(slam_gmapping/gmapping)
+subdirs(turtlebot3)
