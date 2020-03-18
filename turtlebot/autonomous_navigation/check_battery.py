b00a53a825d5a37d6a49842c276cf361f025861f
celinadong <celinady25@gmail.com>
2020-03-18 13:27:41 +0000

    Updated to read locations from json file

--- turtlebot/autonomous_navigation/check_battery.py
+++ turtlebot/autonomous_navigation/check_battery.py
@@ -9,7 +9,7 @@ import math, json
 # Beeping = 11.01V
 # 11.49 V - after 35mins of moving forward with teleop = 546m
 # Maximum translational velocity 0.26 m/s
-global pos_x, pos_y, base_x, base_y, voltage, distance, go_to_base
+global pos_x, pos_y, base, voltage, distance, go_to_base
 
 rospy.init_node('check_battery')
 
@@ -21,8 +21,7 @@ go_to_base = False
 # Open json file to get coords of charging station
 with open('predefined_locations.txt') as json_file:
     locations = json.load(json_file)
-    base_x = locations['charging_station']['x']
-    base_y = locations['charging_station']['y']
+    base = locations['c']
 
 # dest_x, dest_y is always the same (charging station)
 def compute_distance(origin_x, origin_y, dest_x, dest_y):
@@ -47,7 +46,7 @@ while(1):
     rospy.Subscriber('/battery_state', BatteryState, battery_callback)
     rospy.wait_for_message('/battery_state', BatteryState, timeout=1)
 
-    compute_distance(pos_x, pos_y, base_x, base_y)
+    compute_distance(pos_x, pos_y, base[0], base[1])
     print(voltage)
     print(distance)
 
--- turtlebot/autonomous_navigation/main_controller.py
+++ turtlebot/autonomous_navigation/main_controller.py
@@ -11,42 +11,31 @@ in_base = False
 battery_charged = False # Move to window once the battery has been charged
 sunset = False # Turn off the light when the sun has set (plant cycle)
 
-global base_x, base_y, windows
+global base, windows
 windows = []
 
 # Open json file
 with open('predefined_locations.txt') as json_file:
     locations = json.load(json_file)
-    base_x = locations['charging_station']['x']
-    base_y = locations['charging_station']['y']
-    for n in range(1, len(locations['windows'])+1):
-        windows.append((locations['windows'][str(n)]['x'], locations['windows'][str(n)]['y']))
+    base = locations['c']
+    for n in range(1, len(locations['w'])+1):
+        windows.append((locations['w'][str(n)]))
 
+
+if __name__ == "__main__":
+    if check_battery.go_to_base or weather.goToBase:
+        go_to_goal.move(base[0], base[1])
+        in_base = True
+    elif in_base and not(weather.goToBase): # or battery_charged
+        morning_routine
+    # TODO
+    # Create subscriber to the battery messages and when it reaches 12.14V move to windows
+    # If LED light is green, move to windows (morning_routine)
+    
+# For testing
 print('charging station')
-print(base_x)
-print(base_y)
+print(base)
 print('windows')
 print(windows)
-
-go_to_goal.move(base_x, base_y)
-
-# if __name__ == "__main__":
-while(1):
-    if check_battery.go_to_base:
-        go_to_goal.move(base_x, base_y)
-        in_base = True
-    
-        
-# Weather
-# if weather.goToBase:
-#     go_to_goal.move(base_x, base_y)
-# elif not(weather.goToBase):
-#     morning_routine
-
-
-
-# TODO
-# Create subscriber to the battery messages and when it reaches 12.14V move to windows
-# If LED light is green, move to windows (morning_routine)
-# elif in_base == True and battery_charged == True:
+go_to_goal.move(base[0], base[1])
 
