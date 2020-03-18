a2cebe6c6ed26228cf4a614bebfc252bc848bf4f
celinadong <celinady25@gmail.com>
2020-03-18 14:19:15 +0000

    Morning routine with data from light sensors

--- turtlebot/autonomous_navigation/morning_routine.py
+++ turtlebot/autonomous_navigation/morning_routine.py
@@ -1,46 +1,63 @@
 #! /usr/bin/env python
 import rospy, sys
 from nav_msgs.msg import Odometry
+from sensor_msgs.msg import Illuminance
 
 import go_to_goal
 
-global base_x, base_y, windows
+global base, windows
 windows = []
+max_illuminance = 0.0
+max_window = (0.0, 0.0)
 
 # Open json file
 with open('predefined_locations.txt') as json_file:
-    global windows, base_x, base_y
+    global windows, base
     locations = json.load(json_file)
-    base_x = locations['charging_station']['x']
-    base_y = locations['charging_station']['y']
-    for n in range(1, len(locations['windows'])+1):
-        windows.append((locations['windows'][str(n)]['x'], locations['windows'][str(n)]['y']))
-
+    base = locations['c']
+    for n in range(1, len(locations['w'])+1):
+        windows.append((locations['w'][str(n)]))
 
 def compute_distance(origin_x, origin_y, dest_x, dest_y):
     return math.sqrt((dest_x - origin_x)**2 + (dest_y - origin_y)**2)
 
 
-# Move to the window closest to the charging station
-# Move to the next window closest to current window location
+# TODO change depending on the path of each sensor
+def sensor_callback(msg):
+    # Determine which light sensor it is
+    msg.header.frame_id # type string
+    msg.illuminance # type float
 
-not_visited = windows.copy() # Windows not visited
-next_window_x = 0.0
-next_window_y = 0.0
-min_dist = sys.float_info.max # Distance to the closest window
-pos_x = base_x
-pos_y = base_y
+def sensor_listener():
+    rospy.init_node('sensor_readings')
+    rospy.Subscriber('/sensor_msgs', Illuminance, sensor_callback)
+    rospy.wait_for_message('/sensor_msgs', Illuminance, timeout=1)
+
+
+# Move to the window closest to current location
 # Move to windows (sunrise)
 def visit_windows():
+    not_visited = windows.copy() # Windows not visited
+    next_window_x = 0.0
+    next_window_y = 0.0
+    min_dist = sys.float_info.max # Distance to the closest window
+    curr_pos = base
+    avg_lux = 0.0 # Take average of all four light sensors to get the lux at each window
     while (not(not_visited)):
         for w in not_visited:
             # TODO Gather the data from the light sensors
-            distance = compute_distance(pos_x, pos_y)
+            distance = compute_distance(curr_pos[0], curr_pos[1], w[0], w[1])
             if (distance < min_dist):
                 min_dist = distance
                 next_window_x = w[0]
                 next_window_y = w[1]
         go_to_goal.move(w)
+        # TODO
+        # Get the sensor readings
+        sensor_listener()
+    
+
         not_visited.remove(w)
-        pos_x = w[0]
-        pos_y = w[1]
+        curr_pos = (w[0], w[1])
+    # Move to window with max lux
+    go_to_goal.move(max_window)
